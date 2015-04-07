e69878baeee18f3d1f68e792bea2eb24f3fd23c0
Ronyjacobson <Ronyjacobson@gmail.com>
2015-04-07 23:59:32 +0300

    fix

--- Android/src/il/co/onthefly/MainActivity.java
+++ Android/src/il/co/onthefly/MainActivity.java
@@ -13,14 +13,14 @@ public class MainActivity extends FragmentActivity {
 	TabPagerAdapter TabAdapter;
 	ActionBar actionBar;
 	
-	
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    // Inflate the menu items for use in the action bar
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.main_activity_actions, menu);
 	    return super.onCreateOptionsMenu(menu);
-	}
+	}*/
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
